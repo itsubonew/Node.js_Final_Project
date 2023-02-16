e218477938761ac59d2840c2c4a955d7ec868903
itsubonew <itsubonew@gmail.com>
2023-02-15 22:29:11 -0800

    file edited

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require ('bcryptjs');
 const User = require('./models/User.js');
+
 require('dotenv').config();
 const app = express();
 
@@ -22,7 +23,6 @@ app.get('/test',(req,res)=> {
 
 app.post('/register', async (req,res)=> {
     const {name,email,password} = req.body;
-
     try{
     const userDoc = await User.create({
         name,
@@ -32,8 +32,23 @@ app.post('/register', async (req,res)=> {
     res.json({userDoc});
 } catch (e) {
     res.status(422).json(e);
-};
+}
 
-})
+});
+
+app.post('/login', async(req,res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        if(passOk) {
+            res.json('pass ok');
+        }else {
+            res.status(422).json('pass not ok');
+        }
+    } else {
+        res.json('not found');
+    }
+});
 
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
 import {Link} from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 
 export default function LoginPage() {
@@ -8,8 +9,9 @@ export default function LoginPage() {
     async function handleLoginSubmit(ev) {
         ev.preventDefault();
         try{
-        axios.post('/login',{email,password});
-    }catch (e) {
+        await axios.post('/login',{email,password});
+        alert('Login successful');
+    } catch (e) {
         alert('login failed');
     }
 }
@@ -17,7 +19,7 @@ export default function LoginPage() {
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto onSubmit={handleLoginSubmit}">
+                <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
                     <input type="email" 
                     placeholder="your@email.com" 
                     value={email} 
@@ -25,7 +27,7 @@ export default function LoginPage() {
                     <input type="password" 
                     placeholder="password" 
                     value={password} 
-                    onChange={ev => setPassward(ev.target.value)} />    
+                    onChange={ev => setPassword(ev.target.value)} />    
                     <button className="primary">Login</button> 
                 <div className="text-center py-2 text-gray-500">
                     Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
