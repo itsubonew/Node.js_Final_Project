eb58814e368efb7bb1bd1a0a1cb4ff84bf749771
itsubonew <itsubonew@gmail.com>
2023-02-16 22:31:01 -0800

    file edited

--- api/index.js
+++ api/index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require ('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'sdfsjfbwhbfksjfasfkja';
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    proxy: "http://localhost:5173/register",
+    origin: "http://localhost:5173",
 }));
+
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -23,13 +28,14 @@ app.get('/test',(req,res)=> {
 
 app.post('/register', async (req,res)=> {
     const {name,email,password} = req.body;
+
     try{
     const userDoc = await User.create({
         name,
         email,
         password:bcrypt.hashSync(password, bcryptSalt),
     });
-    res.json({userDoc});
+    res.json(userDoc);
 } catch (e) {
     res.status(422).json(e);
 }
@@ -40,10 +46,17 @@ app.post('/login', async(req,res) => {
     const {email,password} = req.body;
     const userDoc = await User.findOne({email});
     if (userDoc) {
-        const passOk = bcrypt.compareSync(password, userDoc.password)
-        if(passOk) {
-            res.json('pass ok');
-        }else {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({
+                email:userDoc.email, 
+                id:userDoc._id,
+                }, 
+                jwtSecret, {}, (err,token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(userDoc);
+            });
+        } else {
             res.status(422).json('pass not ok');
         }
     } else {
@@ -51,4 +64,17 @@ app.post('/login', async(req,res) => {
     }
 });
 
+app.get('/profile', (req,res)=> {
+    const {token} = req.cookies;
+    if(token) {
+    jwt.verify(token, jwtSecret, {}, async(err, userData)=> {
+    if (err) throw err;
+    const {name,email,_id} = await User.findById(userData.id);
+    res.json({name,email,_id});
+});
+    } else {
+        res.json(null);
+    }
+   
+})
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,19 +1,28 @@
-import {Link} from "react-router-dom";
-import { useState } from "react";
+import {Link, Navigate} from "react-router-dom";
+import { useContext, useState } from "react";
 import axios from "axios";
+import {UserContext} from "../UserContext.jsx";
 
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const {setUser} = useContext(UserContext);
     async function handleLoginSubmit(ev) {
         ev.preventDefault();
         try{
-        await axios.post('/login',{email,password});
+        const {data} = await axios.post('/login',{email,password});
+        setUser(data);
         alert('Login successful');
+        setRedirect(true);
     } catch (e) {
         alert('login failed');
     }
+}
+
+if (redirect) {
+    return <Navigate to={'/'} />
 }
     return (
         <div className="mt-4 grow flex items-center justify-around">
@@ -30,7 +39,7 @@ export default function LoginPage() {
                     onChange={ev => setPassword(ev.target.value)} />    
                     <button className="primary">Login</button> 
                 <div className="text-center py-2 text-gray-500">
-                    Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
+                    Don't have an account yet? <Link classsName="underline text-black" to={'/register'}>Register now</Link>
                 </div>
                 </form>
             </div>
